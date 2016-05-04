f4014e91d69ca812c7069b7ee70f2db401c98a28
Seth Low <Seth.Low@MOTIONSOFT.COM>
2016-05-04 11:43:42 -0400

    These are the standard scripts I use on the second of the month to reverse Transactions and create a balancing payment on invoices that have gotten a normal dues fee rather then a freeze fee. The Freeze fee Invoice generator does just that, creates a file to import with the corrected freeze fees.

---
+++ 2nd SOP-Freeze_Invoice_Generator.sql
@@ -0,0 +1,183 @@
+declare @RunDate date = '5/2/2016' --getdate();
+
+IF (OBJECT_ID('tempdb..#FreezeInvoices') IS NOT NULL) DROP TABLE #FreezeInvoices
+
+;with 
+	CTE_TargetAgreements
+		AS (
+				SELECT mp.TxPaymentId, r.PaymentProcessRequestId, mr.MemberAgreementId
+				FROM [TSI_tactical].[dbo].[Storage_May_PaymentProcessRequestData_Remmoval] r ---UPDATE THIS WITH THE PAYMENT PROCESS REQUEST ROW THAT ARE GETTING REVERSED
+				INNER JOIN dbo.MemberAgreementPaymentRequest mp ON MP.TxPaymentId = r.TxPaymentID
+				INNER JOIN dbo.MemberAgreementInvoiceRequest mr ON mr.MemberAgreementInvoiceRequestId = mp.MemberAgreementInvoiceRequestId
+			),			
+	LastSuspensionEnd as
+		(
+			select 
+				ROW_NUMBER() over (partition by [SuspensionId] order by [SuspensionEndDateId] desc) as LastSuspensionEndRN
+				,sed.SuspensionId
+				,convert(date,sed.EndTime) EndTime
+			from 
+				dbo.SuspensionEndDate sed (nolock)
+		),
+	LastSupension as
+		(
+			select
+				ROW_NUMBER() over (partition by sps.TargetEntityId order by sps.BeginTime desc) as LastSupensionRN
+				,sps.TargetEntityId as MemberAgreementId
+				,sps.SuspensionId
+				,convert(date,sps.BeginTime) as BeginTime
+				,spsr.Name
+				,spsr.RecurringFeeItemId
+			from 
+				dbo.Suspension sps (nolock)
+				inner join dbo.SuspensionReason spsr (nolock) on sps.SuspensionReasonId = spsr.SuspensionReasonId
+			where
+				sps.Status in (1,2,3)
+				and sps.BeginTime <= @RunDate
+				and sps.TargetEntityIdType & 8 = 8
+		),
+	NextCancel as
+		(
+			select
+				ROW_NUMBER() over (partition by can.EntityId order by can.Date asc) as NextCancellationRN
+				,can.EntityId as MemberAgreementId
+				,convert(date,can.Date) as CancellationDate
+				,can.EnteredDate as CancellationEnteredDate
+			from 
+				dbo.Cancellation can (nolock)
+			where
+				can.StateId in (1,2,3,4,5)
+				and can.EntityIdType = 1
+		),	
+	LastBillDate as
+		(
+			select
+				ROW_NUMBER() over (partition by mair.MemberAgreementID order by mair.BillDate desc) as LastBillRN
+				,mair.MemberAgreementID
+				,convert(date,mair.BillDate,101) as BillDate
+				,txi.TargetDate
+				,mair.ProcessType
+			from 
+				dbo.MemberAgreementInvoiceRequest mair (nolock)
+				inner join dbo.MemberAgreementInvoiceRequestItem mairi (nolock)on mair.MemberAgreementInvoiceRequestId = mairi.MemberAgreementInvoiceRequestId
+				inner join dbo.MemberAgreementItem mai (nolock) on mairi.MemberAgreementItemId = mai.MemberAgreementItemId
+					and mai.IsKeyItem = 1
+				inner join dbo.TxInvoice txi (nolock) on mair.TxInvoiceId = txi.TxInvoiceID
+
+				--INNER JOIN tsi_tactical.[dbo].[Storage_Feb_Freeze_InvoiceIssue_FINAL] fin (NOLOCK) ON fin.MemberAgreementId = mair.MemberAgreementID
+			where 1=1
+				and isnull(mair.ProcessType,0) != 1
+				and mair.TxInvoiceId is not null
+				and convert(date,mair.BillDate) <= @RunDate
+		)
+select
+	bu.Code as LocationCode
+	,bu.Name as LocationName
+	,pr.RoleID as MemberID
+	,ma.MemberAgreementID
+	,convert(date,ma.StartDate) as StartDate
+	,convert(date,ma.EditableStartDate) as EditableStartDate
+	,replace(replace(replace(agr.Description,char(9),''),char(13),''),char(10),'') as AgreementName
+	,replace(replace(replace(bun.Name,char(9),''),char(13),''),char(10),'')  as BundleName
+	,sm.Name as AgreementStatus
+	,maip.Price as KeyItemPrice
+	,lbd.BillDate as LastBillDate
+	,lbd.ProcessType as LastBillType
+	,sch.Name as BillingSchedule
+	,lsps.BeginTime as LastSuspensionBegin
+	,lspse.EndTime as LastSuspensionEnd
+	,lsps.Name as SuspensionReason
+	,lsps.RecurringFeeItemId as SuspensionFeeItemID
+	,itm.name as SuspensionFeeItem
+	,coalesce(ip_bu.Price,ip_div.Price,ip_ent.Price,0) as FreezeFeePrice
+	,nc.CancellationDate
+	, itm.UPC AS ItemCode
+	,@RunDate as AnalysisDate
+INTO #FreezeInvoices
+from
+	dbo.MemberAgreement ma (nolock)
+	INNER JOIN CTE_TargetAgreements ta ON ta.MemberAgreementId = ma.MemberAgreementId
+	--INNER JOIN TSI_Tactical.[dbo].[Storage_March_1st_PaymentProcessRequest_Removal] s ON s.
+	--INNER JOIN tsi_tactical.[dbo].[Storage_Feb_Freeze_InvoiceIssue_FINAL] fin (NOLOCK) ON fin.MemberAgreementId = ma.MemberAgreementID
+
+	inner join dbo.PartyRole pr (nolock) on ma.PartyRoleId = pr.PartyRoleID	
+	inner join dbo.StatusMap sm (nolock) on ma.Status = sm.StatusId 		and sm.StatusMapType = 5
+	inner join dbo.Agreement agr (nolock) on ma.AgreementId = agr.AgreementId 
+	inner join dbo.BusinessUnit bu (nolock) on ma.BusinessUnitId = bu.BusinessUnitId
+	inner join dbo.MemberAgreementItem mai (nolock) on ma.MemberAgreementId = mai.MemberAgreementId 		and mai.IsKeyItem = 1
+	inner join dbo.MemberAgreementItemPerpetual maip (nolock) on mai.MemberAgreementItemId = maip.MemberAgreementItemId 		and maip.BillCount is null
+	inner join dbo.BillingSchedule bs (nolock) on maip.BillingScheduleId = bs.BillingScheduleId
+	left join dbo.Schedule sch (nolock) on bs.ScheduleId = sch.ScheduleId
+	left join dbo.Bundle bun (nolock) on mai.BundleId = bun.BundleId
+	left join LastBillDate lbd on ma.MemberAgreementId = lbd.MemberAgreementId 		and lbd.LastBillRN = 1
+	left join LastSupension lsps on ma.MemberAgreementId = lsps.MemberAgreementId 		and lsps.LastSupensionRN=1
+	left join LastSuspensionEnd lspse on lsps.SuspensionId = lspse.SuspensionId 		and lspse.LastSuspensionEndRN=1
+	left join NextCancel nc on ma.MemberAgreementId = nc.MemberAgreementId 		and nc.NextCancellationRN=1
+	left join dbo.Item itm (nolock) on lsps.RecurringFeeItemId = itm.ItemID 	
+	left join dbo.ItemPrice ip_bu (nolock) on lsps.RecurringFeeItemId = ip_bu.ItemID
+		and ip_bu.SourceID = bu.BusinessUnitId
+		and ip_bu.SourceLevel = 3
+		and @RunDate between ip_bu.StartDate_UTC and isnull(ip_bu.EndDate_UTC,@RunDate)
+	left join dbo.ItemPrice ip_div (nolock) on lsps.RecurringFeeItemId = ip_div.ItemID
+		and ip_div.SourceID = bu.DivisionId
+		and ip_div.SourceLevel = 2
+		and @RunDate between ip_div.StartDate_UTC and isnull(ip_div.EndDate_UTC,@RunDate)
+	left join dbo.ItemPrice ip_ent (nolock)  on lsps.RecurringFeeItemId = ip_ent.ItemID
+		and ip_ent.SourceID = 0
+		and ip_ent.SourceLevel = 1
+		and @RunDate between ip_ent.StartDate_UTC and isnull(ip_ent.EndDate_UTC,@RunDate)
+
+where 1=1
+	and agr.AgreementTypeId = 1  -- Memberships Only
+	and pr.PartyRoleTypeID = 1 -- Members Only
+	and lsps.SuspensionId is not null -- there was a suspension in effect during date being analyzed
+	and isnull(lspse.EndTime,dateadd(day,1,@RunDate)) >= dateadd(day,1,@RunDate) -- the end date of the suspension is past the billing date being analyzed
+	and ((nc.CancellationDate is null) or (convert(date,nc.CancellationDate) > @RunDate))  -- if agreement was to be cancelled, it was after the date being analyzed
+	and coalesce(ip_bu.Price,ip_div.Price,ip_ent.Price,0) != 0 -- There is a freeze fee assigned
+	and lbd.BillDate <= @RunDate -- The last bill date is prior to the analysis date
+	--and lbd.BillDate >= @RunDate -- The last bill date on the analysis date
+	and sch.Name = 'Monthly on 1st'  -- we are only converned about memberships billing on the first
+
+
+
+
+-->>> THIS WILL GENERATE THE INVOICES TO IMPORT FILE
+SELECT 
+		'' AS InvoiceID
+		, MemberID AS OwnerID
+		, 'Primary Account' AS AccountName
+		, SuspensionFeeItem AS ItemName
+		, 1 AS Quantity
+		, FreezeFeePrice AS Price
+		, 'On Account' AS TenderTypeName
+		, '' AS GiftCardNumber
+		, '' ActivityExpirationDate
+		, 'Freeze Fee' InvoiceComment
+		, ItemCode
+		, LocationCode
+		, 1 AS Sequence
+FROM #FreezeInvoices
+
+
+/*
+
+SELECT '' AS InvoiceId
+		, a.MemberID AS OwnerID
+		, 'Primary Account' AS AccountName
+		, SuspensionFeeItemName AS ItemName
+		, 1 AS Quantity
+		, FeePriceConfig
+		, 'On Account' AS TenderTypename
+		, '' AS GiftCardNumber
+		, '' AS ActivityExpirationDate
+		, 'Freeze Fee' AS InvoiceComment
+		, i.UPC AS ItemCode
+		, b.Code AS LocationCode
+		, 1 AS [Sequence]
+FROM TSI_Tactical.dbo.Storage_March_SuspensionReversal a
+LEFT JOIN Tenant_TSI.dbo.Item i (NOLOCK) ON i.ItemID = a.SuspensionFeeItemID
+LEFT JOIN Tenant_TSI.dbo.BusinessUnit b (NOLOCK) ON b.BusinessUnitId = a.BusinessUnitId
+--WHERE a.MemberID = '38929'
+*/
+
+
---
+++ 2nd SOP-Transaction_Reverse_Script.sql
@@ -0,0 +1,320 @@
+
+/*
+Author: Seth Low
+Date: February 3, 2016
+Purpose: This was designed to reverse a payment  and then all transactions on a group of invoices. 
+
+********* IF this is getting used for another project, you'll have to change the root staging table on all queries *********
+*/
+
+
+DECLARE @Update INT = 0,
+		@MemberID VARCHAR(10) = NULL,
+		@Validate INT = 1,
+		@TxInvoiceID INT = null
+
+
+		;
+
+
+
+--IF (OBJECT_ID('TSI_Tactical.dbo.[Storage_March_Freeze_InvoiceIssue_FINAL]') IS NOT NULL) DROP TABLE [TSI_tactical].dbo.[Storage_March_Freeze_InvoiceIssue_FINAL]
+IF (Object_ID('tempdb..#Reverselist') IS NOT NULL) DROP TABLE #Reverselist;
+IF(Object_ID('tempdb..#ClientAccountStaging') IS NOT NULL) DROP TABLE #ClientAccountStaging;
+IF(Object_ID('tempdb..#Temp') IS NOT NULL) DROP TABLE #Temp
+IF(Object_ID('tempdb..#TransactionStaging') IS NOT NULL) DROP TABLE #TransactionStaging;
+IF(Object_ID('tempdb..#Staging') IS NOT NULL) DROP TABLE #Staging;
+IF(Object_ID('tempdb..#Exclusion') IS NOT NULL) DROP TABLE #Exclusion;
+
+
+SELECT	i.PartyRoleID
+		, p.RoleID AS MemberID
+		, r.TxPaymentID
+		, t.TxInvoiceID
+		, t.TargetBusinessUnitId AS txtBusinessUnitID
+		, t.Amount AS SaleAmount
+		, pv.[First Name]
+		, pv.[Last Name]
+		, mr.MemberAgreementId
+		, a.Name AS Agreement
+INTO #Staging
+--SELECT *
+FROM TSI_Tactical.dbo.Storage_May_PaymentProcessRequestData_Remmoval r (NOLOCK) 
+INNER JOIN TxTransaction t (NOLOCK) ON t.ItemId = r.TxPaymentID AND t.txtypeId = 4
+INNER JOIN TxInvoice i (NOLOCK) ON i.TxInvoiceId = t.TxInvoiceID 
+INNER JOIN PartyRole p (NOLOCK) ON p.PartyRoleId = i.PartyRoleID
+INNER JOIN PartyPropertiesReportingView pv (NOLOCK) ON pv.PartyId= p.PartyID
+LEFT JOIN MemberAgreementInvoiceRequest mr (NOLOCK) ON mr.TxInvoiceId = i.TxInvoiceID
+LEFT JOIN MemberAgreement m (NOLOCK) ON mr.MemberAgreementId = m.MemberAgreementId
+LEFT JOIN Agreement a (NOLOCK) ON  a.AgreementID = m.AgreementId
+
+--SELECT *
+--UPDATE s SET s.TxPaymentId = NULL
+--FROM TSI_Tactical.dbo.Storage_March_SuspensionReversal s
+--WHERE TxPaymentID = 'NULL'
+
+/*
+2732817
+8398774
+8421290
+2251915
+1188267
+*/	
+
+
+			SELECT MemberID
+					, pa.PartyRoleID
+					, Max(cap.ClientAccountId) AS ClientAccountId
+			INTO #ClientAccountStaging
+			FROM #Staging f
+			INNER JOIN PartyRole pa ON pa.RoleId = f.MemberID
+			INNER JOIN ClientAccountParty cap ON cap.PartyId = pa.PartyID
+			INNER JOIN ClientAccount ca ON ca.ClientAccountId = cap.ClientAccountId 
+			WHERE ca.IsActive =1
+					AND cap.IsActive = 1
+			GROUP BY MemberID, pa.PartyRoleId
+
+--SELECT * FROM #ClientAccountStaging
+
+		-->>> Generating Payment to remove initial transaction
+
+		;
+		-->>> These sponsor transfers have no paymentprocessrequest 
+		SELECT MemberID,COUNT(MEMBERID) AS Count_Member
+		INTO #Exclusion
+		FROM #Staging 
+			WHERE 1=1
+				AND MemberID NOT IN (21534858)
+		GROUP BY MemberID
+		HAVING COUNT(MEMBERID) > 1
+		ORDER BY MemberID DESC
+
+	--->>> PAYMENT INSERT <<<---
+		SELECT 
+				DISTINCT 
+				ISNULL(p.TargetBusinessUnitID, f.txtBusinessUnitId) AS TargetBusinessUnitID
+				, GETDATE() AS TargetDate
+				, NULL AS CreditCardTypeID
+				, NULL AS Reference
+				, ISNULL(CAST(p.TxPaymentID AS VARCHAR(20)), 'No TxPaymentID') AS Comments
+				, (CASE	t.IsAccountingCredit
+						WHEN 0 THEN ISNULL(P.Amount, f.SaleAmount) 
+						ELSE ISNULL(-P.Amount, -f.SaleAmount)
+						END) AS Amount
+				, 'EDT' AS TargetDate_ZoneFormat
+				, ISNULL(p.PartyRoleID, cs.PartyRoleId) AS PartyRoleID
+				, -1 AS WorkUnitID 
+				, 0 AS IsReversal
+				, 132 AS TenderTypeID 
+				, NULL AS VoidReasonID
+				, ISNULL(p.ClientAccountId, cs.ClientAccountID) AS ClientAccountID
+				, NULL AS LinkTypeID
+				, NULL AS LinkID 
+				, NULL AS DeclineReasonID
+				, 0 AS IsDeclined
+				, GETUTCDATE() AS TargetDate_UTC
+				, ISNULL(p.IsMultiInvoice, 0) AS IsMultiInvoice
+				, NULL AS TxPaymentGroupID
+				, NULL AS FromBilling
+		INTO #Temp
+		FROM #Staging f --WHERE TxPaymentID IS NULL
+		INNER JOIN #ClientAccountStaging cs ON f.MemberID = cs.MemberID
+		LEFT JOIN dbo.TxPayment p (NOLOCK) ON p.TxPaymentID = f.TxPaymentID
+		LEFT JOIN dbo.TxTransaction t (NOLOCK) ON t.ItemID = p.TxPaymentID AND t.LinkTypeId = 17
+		LEFT JOIN #Exclusion e (NOLOCK) ON e.MemberID = f.MemberID
+		WHERE t.TxTransactionID IS NULL -- This null prevents the credit transfers from getting included.
+			AND IIF(@TxInvoiceID IS NULL, f.TxInvoiceID, @TxInvoiceID) = f.TxInvoiceID
+			AND e.MemberID IS NULL
+
+
+
+IF(@Update = 1)
+	BEGIN
+		-->>> INSERT Payment	
+		INSERT INTO TxPayment
+		SELECT NEWID() AS ObjectID, *
+		FROM #Temp
+	END
+
+
+
+	--->>> PAYMENT TRANSACTION INSERT <<<---
+		
+
+
+		SELECT DISTINCT 
+			t.TxInvoiceId
+			, GETDATE() AS TargetDate
+			, t.TargetDate AS TransactionDate
+			--, t.ItemID AS OriginalPayment --ADDED BY SETH
+			, 4 AS TxTypeID
+			, NULL AS Quantity
+			, CONCAT('ID: ',p.TxPaymentID, ' Is Reversing PaymentID: ', t.ItemID,' These are getting reversed due to Freeze fee issue(StatusUpdate TimeOut).') AS [Description]
+			, NULL AS UnitPrice
+			, t.Amount
+			, 'This is due to StatusUpdate failure, which caused the agreement not to get frozen.' AS Comments
+			, t.TargetDate_ZoneFormat
+			, (SELECT MAX(DISPLAYORDER) + 1 FROM TxTransaction a WHERE a.TxInvoiceID = t.TxInvoiceId) AS DisplayOrder
+			, t.GroupId
+			, IIF(CAST(p.TxPaymentId AS INT) IS NULL, t.ItemID, p.TxPaymentID) AS ItemID
+			, -1 AS WorkUnitID
+			, IIF(t.IsAccountingCredit = 0, 1, 0) AS IsAccountingCredit
+			, 0 AS PriceID
+			, NULL as BundleID
+			, t.BundleGroupId
+			, t.TargetBusinessUnitId
+			, NULL AS PriceIdType
+			, t.LinkTypeId
+			, t.LinkId
+			, GETUTCDATE() AS TargetDate_UTC
+			, t.SalesPersonPartyRoleId
+			, NULL AS RecurringDiscount 
+			, NULL AS [PIFInstallmentDiscount]
+		INTO #TransactionStaging
+		FROM #Staging f (NOLOCK)
+		INNER JOIN TxTransaction t (NOLOCK) ON t.TxInvoiceId = f.TxInvoiceId AND t.TxTypeId = 4
+		LEFT JOIN TxPayment p (NOLOCK) ON p.Comments = CAST(t.ItemID AS varchar(20)) ---THIS WILL NEED TO GET CHANGED TO INNER PRIOR TO BEING SUBMITTED
+		LEFT JOIN TXTransaction t2 (NOLOCK) ON t2.ItemID = t.ItemID AND t2.LinkTypeId = 17
+		WHERE 1=1
+			AND CONVERT(DATE,t.TargetDate,101) = '3/1/2016'
+			AND t.ItemID IS NOT NULL
+			AND t2.TxTransactionID IS NULL -->>> This null excludes credit transfers
+			AND IIF(@TxInvoiceID IS NULL, f.TxInvoiceID, @TxInvoiceID) = f.TxInvoiceID
+		GROUP BY 
+			t.TxInvoiceid
+			, t.Amount
+			, t.TargetDate_ZoneFormat
+			, t.GroupID
+			, t.ItemID
+			, t.BundleGroupID
+			, t.TargetBusinessUnitID
+			, t.LinkTypeId
+			, t.LinkID
+			, t.SalesPersonPartyRoleId
+			, t.[Description]
+			, t.IsAccountingCredit
+			, p.TxPaymentId
+			, t.TargetDate
+		ORDER BY t.TargetDate DESC
+
+IF (@Update = 1)
+	BEGIN
+	-->>> Transaction for Payment
+		INSERT INTO dbo.TxTransaction
+			(ObjectId, TxInvoiceId, TargetDate, TxTypeId, Quantity, Description, UnitPrice, Amount, Comments, TargetDate_ZoneFormat, DisplayOrder, GroupId, ItemId, WorkUnitId, IsAccountingCredit, PriceId, BundleId, BundleGroupId, TargetBusinessUnitId, PriceIdType, LinkTypeId, LinkId, TargetDate_UTC, SalesPersonPartyRoleId, RecurringDiscount, PIFInstallmentDiscount)
+		SELECT NEWID() AS ObjectID
+			, TxInvoiceId
+			, TargetDate
+			, TxTypeId
+			, Quantity
+			, Description
+			, UnitPrice
+			, Amount
+			, Comments
+			, TargetDate_ZoneFormat
+			, DisplayOrder
+			, GroupId
+			, ItemId
+			, WorkUnitId
+			, IsAccountingCredit
+			, PriceId
+			, BundleId
+			, BundleGroupId
+			, TargetBusinessUnitId
+			, PriceIdType
+			, LinkTypeId
+			, LinkId
+			, TargetDate_UTC
+			, SalesPersonPartyRoleId
+			, RecurringDiscount
+			, PIFInstallmentDiscount
+		FROM #TransactionStaging t
+	END
+
+
+
+		IF(Object_ID('tempdb..#RemainingTransactions') IS NOT NULL) DROP TABLE #RemainingTransactions;
+
+
+	--->>> TRANSACTION INSERT <<<---
+
+		SELECT 
+				NEWID() AS ObjectId
+				, t.TxInvoiceId
+				, GETDATE() AS TargetDate
+				, t.TxTypeId
+				, t.Quantity
+				, CONCAT(t.Description, ' - Reversed due to Freeze Fee failure') AS Description
+				, t.UnitPrice
+				, t.Amount
+				, t.Comments
+				, t.TargetDate_ZoneFormat
+				, (SELECT MAX(DISPLAYORDER) + 1 FROM TxTransaction a WHERE a.TxInvoiceID = f.TxInvoiceId) AS DisplayOrder
+				, t.GroupId
+				, t.ItemId
+				, -1 AS WorkUnitId
+				, IIF(t.IsAccountingCredit = 0, 1, 0) AS IsAccountCredit
+				, t.PriceId
+				, t.BundleId
+				, t.BundleGroupId
+				, t.TargetBusinessUnitId
+				, t.PriceIdType
+				, t.LinkTypeId
+				, t.LinkId
+				, GETUTCDATE() AS TargetDate_UTC
+				, t.SalesPersonPartyRoleId
+				, t.RecurringDiscount
+				, t.PIFInstallmentDiscount
+		INTO #RemainingTransactions
+		FROM #Staging f (NOLOCK)
+		INNER JOIN TxTransaction t (NOLOCK) ON t.TxInvoiceID = f.TxInvoiceID
+		WHERE 1=1
+			--AND t.txInvoiceId = 18694286
+			AND t.TxtypeID NOT IN (100,4)
+			AND IIF(@TxInvoiceID IS NULL, f.TxInvoiceID, @TxInvoiceID) = f.TxInvoiceID
+
+
+IF (@Update = 1)
+	BEGIN
+-->>> Reverse Remaining Transactions
+		INSERT INTO dbo.TxTransaction
+			(ObjectId, TxInvoiceId, TargetDate, TxTypeId, Quantity, Description, UnitPrice, Amount, Comments, TargetDate_ZoneFormat, DisplayOrder, GroupId, ItemId, WorkUnitId, IsAccountingCredit, PriceId, BundleId, BundleGroupId, TargetBusinessUnitId, PriceIdType, LinkTypeId, LinkId, TargetDate_UTC, SalesPersonPartyRoleId, RecurringDiscount, PIFInstallmentDiscount)
+		SELECT *
+		FROM #RemainingTransactions
+
+	END
+
+
+
+
+IF (@Update = 0)
+	BEGIN	
+		
+		SELECT * FROM #Exclusion
+		
+		SELECT p.RoleID, s.* 
+		--INTO TSI_Tactical.dbo.Storage_May_TransactionsBeingReversed
+		FROM #Staging  s
+		LEFT JOIN PartyRole p ON s.PartyRoleId = p.PartyRoleID
+		WHERE 1=1
+			AND s.MemberID NOT IN (SELECT MEMBERID FROM #Exclusion)
+			AND TxInvoiceID = IIF(@TxInvoiceID IS NOT NULL, @TxInvoiceID, TxInvoiceID)
+
+			
+		--SELECT s.TxPaymentId, p.PaymentProcessRequestId
+		--FROM #Staging s
+		--LEFT JOIN PaymentProcessRequest p ON p.TxPaymentId = s.TxPaymentId
+		--WHERE s.MemberID IN (SELECT MEMBERID FROM #Exclusion) 
+
+		
+		SELECT * FROM #Temp -->> New Payment
+		SELECT * FROM #TransactionStaging -->> Reversing Payment Transaction
+		SELECT * FROM #RemainingTransactions -->> Reversal of remaining transactions
+	END
+
+
+IF (@Validate = 1)
+	BEGIN
+		SELECT * FROM TxTransaction WHERE TxInvoiceID = @TxInvoiceID
+	END
+
