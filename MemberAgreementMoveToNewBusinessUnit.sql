0ea8846b2531b4d78a5499ee7d3ad7c300a1b7b3
Seth Low <Seth.Low@MOTIONSOFT.COM>
2016-07-12 10:00:25 -0400

    Updating for July Moves

--- MemberAgreementMoveToNewBusinessUnit.sql
+++ MemberAgreementMoveToNewBusinessUnit.sql
@@ -2,9 +2,10 @@
 Very Simplistic script intended to move an agreement from one businessunit to another. 
 ********************************************************************************/
 
-UPDATE m SET m.BusinessUnitID = a.MoveToBU
+UPDATE m SET m.BusinessUnitID = a.[New BUID]
 --SELECT m.MemberAgreementID, m.BusinessUnitId, a.MOVETOBU
-FROM TSI_tactical.dbo.Storage_NS132542_SoHo_AgreementMoves a
-INNER JOIN MemberAgreement m on a.MemberAgreementid = m.MemberAgreementid 
-WHERE m.BusinessUnitId = 145
-	AND m.PartyRoleID = a.PARTYROLEID
+--SELECT a.MemberID, m.MemberAgreementID, m.BusinessUnitID, a.[New Buid]
+FROM TSI_tactical.[dbo].[Storage_NS138911_MemberAgreementMove_WallToFiDi] a
+INNER JOIN tenant_TSI.dbo.MemberAgreement m on a.MemberAgreementid = m.MemberAgreementid 
+WHERE m.BusinessUnitId = 161
+
